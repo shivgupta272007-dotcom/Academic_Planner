469fda0949e49fd23e74909b282c4e6756d085c8
Shiv Gupta <shivgupta@Shivs-MacBook-Air.local>
2026-06-03 20:26:25 +0530

    Configure HashRouter, base path, and gh-pages scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './contexts/AppContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { PomodoroProvider } from './contexts/PomodoroContext';
